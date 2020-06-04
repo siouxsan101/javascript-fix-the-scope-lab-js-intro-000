53bd37e738f501c0b6fd64c044ed195700eaf471
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-04 17:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
@@ -11,6 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  var animal = 'cat';
   return animal
 }
 
