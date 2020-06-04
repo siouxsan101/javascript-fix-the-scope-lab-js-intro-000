fca68e2e996cf7fb1ad56d505ce702edc51beaa0
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-04 17:46:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  const two = 2; 
+  const two = 2;
   return n + two
   // Feel free to move things around!
-  
+
 }
 
 var funkyFunction = function() {
